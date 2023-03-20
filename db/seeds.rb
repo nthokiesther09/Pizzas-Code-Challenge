d69c51cbf899aae133864c25d55cb26730a5f637
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-20 13:05:57 +0300

    created migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# generating 12 restaurants
+12.times do
+restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address
+)
+end
+#generating 12 pizzas
+12.times do
+pizza = Pizza.create(
+    name: Faker::Food.dish,
+    ingredients: Faker::Food.ingredient
+)
+end
+#generating 12 random restaurant_pizzas
+12.times do
+restaurant_pizza = RestaurantPizza.create(
+    price: rand(1..30),
+    restaurant_id: rand(1..12),
+    pizza_id: rand(1-12)
+)
+end
