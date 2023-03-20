3606c3d61d2ca55c3994a5fc8c3c1e31c364e87f
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-20 16:36:55 +0300

    updated controllers

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,37 @@
 class RestaurantPizzasController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+      def create
+        restaurant_pizza = RestaurantPizza.create(restaurant_pizza_params)
+        if restaurant_pizza.valid?
+          render json: restaurant_pizza, status: :created
+        else
+          render json: { errors:["validation errors" ]}, status: :unprocessable_entity
+        end
+    
+     
+        # restaurant_pizza = RestaurantPizza.create(restaurant_pizza_params)
+        # render json: restaurant_pizza, status: :created
+
+      end
+
+        private
+        
+        def restaurant_pizza_params
+          params.permit(:price, :pizza_id, :restaurant_id)
+          
+        end
+
+        # def render_uprocessable_entity_response(invalid)
+        #   render json: { errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+        # end
+    
+
+      
+
+
+  
 end
