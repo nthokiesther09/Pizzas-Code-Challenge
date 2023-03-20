3906bead2341b6c390bef7c96827bc9bb732e590
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-20 16:31:18 +0300

    created resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :pizzas, only: [:index]
+  resources :restaurant_pizzas, only: [:create] 
+  
+  resources :restaurants, only: [:index, :show, :destroy]
+  
 end
