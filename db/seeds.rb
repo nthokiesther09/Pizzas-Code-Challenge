53061d5dd11b0e38345d1fea2313be6db13f3ac0
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-20 16:48:19 +0300

    completed code challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,25 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# generating 12 restaurants
-12.times do
+# generating 16 restaurants
+16.times do
 restaurant = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.full_address
 )
 end
-#generating 12 pizzas
-12.times do
+#generating 16 pizzas
+16.times do
 pizza = Pizza.create(
     name: Faker::Food.dish,
     ingredients: Faker::Food.ingredient
 )
 end
-#generating 12 random restaurant_pizzas
-12.times do
+#generating 16 random restaurant_pizzas
+16.times do
 restaurant_pizza = RestaurantPizza.create(
     price: rand(1..30),
-    restaurant_id: rand(1..12),
-    pizza_id: rand(1-12)
+    restaurant_id: rand(1..16),
+    pizza_id: rand(1-16)
 )
 end
