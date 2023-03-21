1f70f048393d1dd5eb4834b96d9dfcb2956c8842
nthokiesther09 <nthokiesther09@gmail.com>
2023-03-21 10:08:43 +0300

    created serializers

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem "sprockets-rails"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
-
+gem 'active_model_serializers'
 
 gem "faker"
 
---
+++ app/serializers/restaurant_pizza_serializer.rb
@@ -0,0 +1,5 @@
+class RestaurantPizzaSerializer < ActiveModel::Serializer
+  attributes :id, :price, :pizza_id, :restaurant_id
+  belongs_to :restaurant
+  belongs_to :pizza
+end
